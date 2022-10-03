27de6796a303004d0e270701e3084913916f41df
Ankur Shukla <itzankurhere@gmail.com>
2022-10-03 17:17:24 +0530

    SignIn, SignUp UI completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
-import ListPage from './Components/List_Page/ListPage';
-
+import "./App.css";
+import ListPage from "./Components/List_Page/ListPage";
+import Login from "./Components/HomePage/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./Components/HomePage/SignUp";
 function App() {
   return (
-    <><ListPage/></>
+    <>
+      {/* <ListPage />
+      <Login /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<SignUp />} />
+          <Route exact path="/List" element={<ListPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
