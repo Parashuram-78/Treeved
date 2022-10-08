4b5769423a067f498241e64a33d4b187c8478037
Devansh Gaur <89275109+Devansh-gaur-1611@users.noreply.github.com>
2022-10-08 12:29:39 +0530

    Navbar ui improved

--- src/Components/Navigation/NavBar.jsx
+++ src/Components/Navigation/NavBar.jsx
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 import LongMenu from "./MenuButton";
 const Navbar = ({ user, image }) => {
+  useEffect(() =>{
+    // const headers = ["Content-Type":"application/json"]
+  },[])
   return (
     <div className="Navbar-box">
       <div className="Avatar">
-        <Avatar src={image}></Avatar>
+        <Avatar src={image} style={{width:"30px", height:"30px"}}></Avatar>
         <h3 className="Navbar_user"> Ankur Shukla {user} </h3>
       </div>
       <div className="Navbar-menu">
